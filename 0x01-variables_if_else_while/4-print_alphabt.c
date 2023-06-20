33ab89f024c9b1cf2994217a536f065c99445bc9
stella victor <victorstella018@gmil.com>
2023-06-19 19:44:03 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,25 @@
-#include <stdio.>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- * main - Entry point
+ * main:  - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-	chart low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low != q)
 	putchar(low);
-
-	for (low ='A'; low <= 'Z';low)
-	putchar(low)
+	}
 	putchar('\n');
-        
-        return (0);
-}	
+	return (0);
+}
