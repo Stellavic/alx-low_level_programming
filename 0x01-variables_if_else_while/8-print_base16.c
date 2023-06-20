e831c6642a1c1a178583beba298001cc95fe4562
stella victor <victorstella018@gmil.com>
2023-06-19 21:22:11 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,8 +14,9 @@ int main(void)
 	int d;
 	char low;
 
-	for (d = 'o'; d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 	putchar(d);
+	
 	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
 	putchar('\n');
