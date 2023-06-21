750bdc4f72021db705987396eea6a11255ae1da5
stella victor <victorstella018@gmil.com>
2023-06-20 23:15:31 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,11 +12,11 @@ int print_sign(int n);
 
 int _abs(int);
 
-int print_last_digit(int);
+int print_last_dihit(int);
 
 void jack_bauer(void);
 
-void time_tables(void);
+void times_table(void);
 
 int add(int, int);
 
